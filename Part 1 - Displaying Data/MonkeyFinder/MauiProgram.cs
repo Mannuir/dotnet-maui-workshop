2eca3a2630571301172c443028b72950a3f8649d
Mannuir <vasarik@gmail.com>
2023-05-27 11:20:34 +0300

    update details page

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -23,6 +23,9 @@ public static class MauiProgram
         builder.Services.AddSingleton<MonkeysViewModel>();
         builder.Services.AddSingleton<MainPage>();
 
+        builder.Services.AddTransient<MonkeyDetailsViewModel>();
+        builder.Services.AddTransient<DetailsPage>();
+
         return builder.Build();
     }
 }
